8eb8329695f10b8144bf2fae9555d74e01f20798
Daavp <118278693+Daavp@users.noreply.github.com>
2023-04-19 17:02:45 -0400

    Correción trycatch

--- Desafio3/srcDesafio3/app.js
+++ Desafio3/srcDesafio3/app.js
@@ -7,30 +7,40 @@ const manager = new ProductManager("./products.json");
 
 // USO DE QUERY
 app.get("/products",async (req,res)=>{
-    const allProducts = await manager.getProducts();
-    const limit = req.query.limit;
-    if(!limit){
-// Todos los productos        
-               return res.send(allProducts);
-            };
-// Limite de producto                       
-    const productsLimit = parseInt(req.query.limit);
-    const productsDisplay = allProducts.filter(u=>u.id <=productsLimit);
-    res.send(productsDisplay);
-    // console.log("Limit: ", productsDisplay)
-    console.log("limit", limit)
+    try {
+        const allProducts = await manager.getProducts();
+        const limit = req.query.limit;
+        if(!limit){
+    // Todos los productos        
+                   return res.send(allProducts);
+                };
+    // Limite de producto                       
+        const productsLimit = parseInt(req.query.limit);
+        const productsDisplay = allProducts.filter(u=>u.id <=productsLimit);
+        res.send(productsDisplay);
+        // console.log("Limit: ", productsDisplay)
+        console.log("limit", limit)
+    } catch (error) {
+        throw new Error(error.message);
+    }
+
 });
 
 // Busqueda de ID PARAMS
 app.get("/products/:pid",async (req,res)=>{
-    const allProducts = await manager.getProducts();
-    const idProduct = parseInt(req.params.pid);
-    const searchProduct = allProducts.find(u=>u.id === idProduct);
-    if(!searchProduct){
-        return res.send(`Producto con el id ${idProduct} no existe, intenta con otro numero.`)
-    };
-    res.send(searchProduct);
-    // console.log("productid", idProduct)
+    try {
+        const allProducts = await manager.getProducts();
+        const idProduct = parseInt(req.params.pid);
+        const searchProduct = allProducts.find(u=>u.id === idProduct);
+        if(!searchProduct){
+            return res.send(`Producto con el id ${idProduct} no existe, intenta con otro numero.`)
+        };
+        res.send(searchProduct);
+        // console.log("productid", idProduct)
+    }
+     catch (error) {
+        throw new Error(error.message);
+    }
 });
 
 app.listen(port,()=>console.log(`Server listening on port ${port}`));
